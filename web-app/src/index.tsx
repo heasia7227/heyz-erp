2e687cedc87f070263ae77f33e86eb2b3d8d06eb
Arthur <108118358+heasia7227@users.noreply.github.com>
2023-07-12 19:22:11 +0800

    init code

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "./routes";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const router = createBrowserRouter(routes);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <RouterProvider router={router} />
+    </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ web-app/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import Home from "@/pages/Home";
+
+const routes = [
+    {
+        path: "/",
+        element: <Home />,
+        // children: [
+        //     {
+        //         path: "/",
+        //         element: <Home />,
+        //     },
+        // ],
+    },
+];
+
+export default routes;
